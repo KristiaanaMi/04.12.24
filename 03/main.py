df651f372e2fea745155a28d7dde96053ea49501
KristiaanaMi <kristiana.mihailova@grobinasskola.lv>
2024-12-04 14:36:10 +0200

    3. uzdevums

--- 03/main.py
+++ 03/main.py
@@ -1,11 +1,11 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]  
 
-num = int(input("Choose a number: "))
+num = int(input("Choose a number: "))  #Prasa, lai ievada skailtli
 
-new_list = []
+new_list = [] #vaido jaunu sarakstu.
 
-for i in a:
+for i in a:    #Izvada skaitļus no a kas doti kursu saskaitot iznāk norāditais skaitlis 
     if i < num:
         new_list.append(i)
 
-print(new_list)
+print(new_list) #Izvada jauno sarakstu.
