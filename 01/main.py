c9113572eda88d9dc368a58e51333fb943da052d
KristiaanaMi <kristiana.mihailova@grobinasskola.lv>
2024-12-04 14:18:42 +0200

    1uzdevums

--- 01/main.py
+++ 01/main.py
@@ -1,11 +1,11 @@
 """ Insert Name """
-name = input("Whats your name: ")
+name = input("Whats your name: ") #izvada jautajumu kads ir vārds.
 
 """ Insert age. Also convert age into an Integer value """
-age = int(input("How old are you: "))
+age = int(input("How old are you: "))  #Izvada ajutajumu par to cik gadi
 
 """ Do the math """
-year = str((2018 - age)+100)
+year = str((2018 - age)+100) # saskaita kad bus 100 gadi cilvekam.
 
 """ Print the result """
-print(name + " will be 100 years old in the year " + year)
+print(name + " will be 100 years old in the year " + year) #Izvada tekstu kur raksits izrēķinātais gads ar vardu kopā.
